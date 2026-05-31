c058d63c52de873ba6b91192b08409eee0926545
huahai0202 <2023217276@mail.hfut.edu.cn>
2026-05-31 15:57:09 +0800

    Add executable version metadata

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Quicker Image Annotator")]
+[assembly: AssemblyDescription("Lightweight Windows image annotation tool")]
+[assembly: AssemblyCompany("huahai0202")]
+[assembly: AssemblyProduct("Quicker Image Annotator")]
+[assembly: AssemblyCopyright("Copyright (c) huahai0202")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("63f7dd3f-7f12-4c32-86da-9cb98c2a59b7")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyInformationalVersion("1.0.6")]
